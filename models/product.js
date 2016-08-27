18620496b4b245ca0b7fa2ee065cd25bd18179d6
Royal Pinto <royalpinto@gmail.com>
2016-08-27 18:39:22 +0530

    Added initial product model.

---
+++ models/product.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+
+
+var ProductSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+});
+
+
+module.exports = mongoose.model('Product', ProductSchema);
