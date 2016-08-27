cd58a90473bb0327302c60ef535045b0ed94090e
Royal Pinto <royalpinto@gmail.com>
2016-08-27 18:38:08 +0530

    Connected products route to the app.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var paginate = require('express-paginate');
 
 var users = require('./routes/users');
+var products = require('./routes/products');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(paginate.middleware(10, 50));
 
 app.use('/users', users);
+app.use('/products', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
