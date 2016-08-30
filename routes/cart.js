f3a6180209b7c2d3b04b3299d65d64bd1001ff64
Royal Pinto <royalpinto@gmail.com>
2016-08-30 19:48:14 +0530

    Added /api/cart/items/ service API doc.

--- routes/cart.js
+++ routes/cart.js
@@ -23,7 +23,16 @@ router.use('/items/*/',
     }
 );
 
-
+/**
+ * @api {post} /api/cart/items/ Add product to the cart
+ * @apiName AddToCart
+ * @apiGroup Cart
+ *
+ * @apiParam {String} productId Product id
+ * @apiParam {Number} quantity Quantity
+ *
+ * @apiSuccess (Success 204) {Header} 204 No Content
+ */
 router.post('/items/', function(req, res, next) {
     var cart = req.cart;
     Product.findById(req.body.productId)
@@ -45,12 +54,34 @@ router.post('/items/', function(req, res, next) {
     ;
 });
 
-
+/**
+ * @api {get} /api/cart/items/ Get cart items
+ * @apiName GetCartItems
+ * @apiGroup Cart
+ *
+ * @apiSuccess (Success 200) {Object[]} products Products added to the cart
+ * @apiSuccess (Success 200) {Number} products.id Cart item id
+ * @apiSuccess (Success 200) {String} products.productId Product id
+ * @apiSuccess (Success 200) {String} products.name Product name
+ * @apiSuccess (Success 200) {Number} products.price Product price
+ * @apiSuccess (Success 200) {Number} products.quantity Product quantity
+ */
 router.get('/items/', function(req, res) {
     res.json(req.cart.items);
 });
 
-
+/**
+ * @api {get} /api/cart/items/id/ Get cart item
+ * @apiName GetCartItem
+ * @apiGroup Cart
+ *
+ * @apiSuccess (Success 200) {Object[]} products Products added to the cart
+ * @apiSuccess (Success 200) {Number} products.id Cart item id
+ * @apiSuccess (Success 200) {String} products.productId Product id
+ * @apiSuccess (Success 200) {String} products.name Product name
+ * @apiSuccess (Success 200) {Number} products.price Product price
+ * @apiSuccess (Success 200) {Number} products.quantity Product quantity
+ */
 router.get('/items/:_id/', function(req, res) {
     var item = req.cart.items.id(req.params._id);
     if (!item) {
@@ -59,7 +90,15 @@ router.get('/items/:_id/', function(req, res) {
     res.json(item);
 });
 
-
+/**
+ * @api {put} /api/cart/items/id/ Update product quantity
+ * @apiName UpdateCartItem
+ * @apiGroup Cart
+ *
+ * @apiParam {String} quantity Quantity to be updated
+ *
+ * @apiSuccess (Success 204) {Header} 204 No Content
+ */
 router.put('/items/:_id/', function(req, res, next) {
     var cart = req.cart;
     var item = cart.items.id(req.params._id);
@@ -76,7 +115,13 @@ router.put('/items/:_id/', function(req, res, next) {
     });
 });
 
-
+/**
+ * @api {delete} /api/cart/items/id/ Delete product from the cart
+ * @apiName RemoveFromCart
+ * @apiGroup Cart
+ *
+ * @apiSuccess (Success 204) {Header} 204 No Content
+ */
 router.delete('/items/:_id/', function(req, res, next) {
     var cart = req.cart;
     var item = cart.items.id(req.params._id);
