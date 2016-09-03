4f0afba58636eff8d2d30009c51c0919f996145e
Royal Pinto <royalpinto@gmail.com>
2016-09-03 19:24:47 +0530

    Created a app module which exports app server.

---
+++ app/index.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var url = require('url');
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var router = require('./router');
+var models = require('./../models');
+
+var approuter = new router.Router();
+
+// var users = require('./../routes/users');
+
+approuter.use(function(req, res, next) {
+    req.query = querystring.parse(url.parse(req.url).query);
+    next();
+});
+approuter.use(bodyParser.json());
+approuter.use(bodyParser.urlencoded({extended: false}));
+approuter.use(cookieParser());
+approuter.use(session({
+    secret: 'sdasdsda',
+    saveUninitialized: false,
+    resave: true,
+    store: new MongoStore({
+        db: models.db,
+    }),
+}));
+
+// approuter.use(/^\/users\//, users);
+// approuter.use(/^\/users\//, users);
+
+var server = http.createServer(function(req, res) {
+    approuter.dispatch(req, res, function() {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('Not Found');
+    });
+});
+
+
+module.exports = server;
