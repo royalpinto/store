7faace084dcbe9b9b49010ed595139aec9d8e3ad
Royal Pinto <royalpinto@gmail.com>
2016-09-05 21:04:20 +0530

    Added cart model test script with an initial validation test case.

---
+++ test/models/cart.js
@@ -0,0 +1,71 @@
+/* global describe, it, before, beforeEach, afterEach */
+
+var chai = require('chai');
+var mongodb = require('mongodb');
+var config = require('./../../config');
+var models = require('./../../models');
+var errors = require('./../../errors');
+
+
+describe('Cart(Model):', function() {
+    before(function(done) {
+        mongodb.MongoClient.connect(config.db.uri)
+        .then(function(db) {
+            return models.init(db);
+        })
+        .then(function() {
+            done();
+        })
+        .catch(done)
+        ;
+    });
+
+    var cleanCollection = function(done) {
+        Promise.all([
+            models.db.collection(models.Cart.collectionName).removeMany(),
+            models.db.collection(models.User.collectionName).removeMany(),
+            models.db.collection(models.Permission.collectionName).removeMany(),
+        ])
+        .then(function() {
+            done();
+        })
+        ;
+    };
+
+    beforeEach(cleanCollection);
+    afterEach(cleanCollection);
+
+    it('It should fail the validation.', function(done) {
+        var user = new models.User({
+            name: "Lohith Royal Pinto",
+            email: "royalpinto@gmail.com",
+            username: "royalpinto",
+            group: "member",
+        });
+        user.save()
+        .then(function() {
+            var cart = new models.Cart({
+                userId: user._id,
+                items: [],
+            });
+            var cartitem = new models.CartItem({
+                productId: null,
+                quantity: 90,
+            });
+            cart.items.push(cartitem);
+            return cart.validate();
+        })
+        .then(function() {
+            done("Validation should have failed.");
+        })
+        .catch(function(error) {
+            chai.assert.instanceOf(error, errors.ValidationError,
+                "Validation should return an array.");
+            done();
+        })
+        .catch(function(errors) {
+            done(errors);
+        })
+        ;
+    });
+});
