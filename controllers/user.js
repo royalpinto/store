12d7137d6ec0b29af169d8c48aedc97cd9892bf7
Royal Pinto <royalpinto@gmail.com>
2016-09-05 15:14:50 +0530

    Updated user controller to use base class controller.

--- controllers/user.js
+++ controllers/user.js
@@ -1,60 +1,21 @@
+var util = require('util');
 var models = require('./../models');
+var Controller = require('./controller');
 
 
-var createUser = function(data) {
-    // Set/override the group as member.
-    data = data || {};
-    data.group = 'member';
-
-    var user = new models.User(data);
-    return user.save()
-    .then(function() {
-        return user;
-    });
+var UserController = function() {
+    Controller.call(this, models.User);
 };
 
-var readUser = function(id) {
-    return models.User
-    .findById(id)
-    ;
+util.inherits(UserController, Controller);
+
+Controller.prototype.create = function() {
+    throw new Error("Not allowed, please use auth controller.");
 };
 
-var readUsers = function(query, limit, skip, order) {
-    return models.User
-    .find(query)
-    .sort(order || {})
-    .limit(limit)
-    .skip(skip || 0)
-    .toArray()
-    ;
-};
-
-var updateUser = function(id, data) {
-    // Set/override the group as member.
-    data = data || {};
-    data.group = 'member';
-
-    return models.User
-    .findById(id)
-    .then(function(user) {
-        return user.update(data);
-    });
-};
-
-var removeUser = function(id) {
-    return models.User
-    .findById(id)
-    .then(function(user) {
-        return user.remove();
-    })
-    ;
+Controller.prototype.update = function() {
+    throw new Error("Not allowed, please use auth controller.");
 };
 
 
-module.exports = {
-    createUser: createUser,
-    readUser: readUser,
-    readUsers: readUsers,
-    updateUser: updateUser,
-    removeUser: removeUser,
-};
+module.exports = new UserController();
