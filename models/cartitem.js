9cffc347923a105dd0b4965154ec764cc9d64020
Royal Pinto <royalpinto@gmail.com>
2016-09-05 19:56:04 +0530

    Added CartItem model.

---
+++ models/cartitem.js
@@ -0,0 +1,32 @@
+var util = require('util');
+var mongodb = require('mongodb');
+var Model = require('./model');
+var validators = require('./validators');
+
+
+var schema = {
+    productId: {
+        type: mongodb.ObjectID,
+        validations: [{
+            fn: validators.required,
+        }],
+    },
+    quantity: {
+        type: Number,
+        validations: [{
+            fn: validators.number(1),
+        }],
+    },
+};
+
+var CartItem = function CartItem(properties) {
+    Model.call(this, properties);
+};
+
+util.inherits(CartItem, Model);
+Object.assign(CartItem, Model);
+
+CartItem.setSchema(schema);
+
+
+module.exports = CartItem;
