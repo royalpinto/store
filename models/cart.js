712e23aeb4823e1b84641414003847f7f3dc568d
Royal Pinto <royalpinto@gmail.com>
2016-09-05 20:58:53 +0530

    Updated cart model validation to return a promise.

--- models/cart.js
+++ models/cart.js
@@ -12,20 +12,27 @@ var schema = {
         type: Array,
         validations: [{
             fn: function(items, key) {
-                if (!(items instanceof Array)) {
-                    return util.format("%s is invalid.", key);
-                }
-                var i;
-                for (i = 0; i < items.length; i++) {
-                    var item = items[i];
-                    if (!(item instanceof CartItem)) {
-                        return util.format("%s[%d] is invalid.", key, i);
+                return new Promise(function(resolve, reject) {
+                    if (!(items instanceof Array)) {
+                        return reject(util.format("%s is invalid.", key));
                     }
-                    var r = item.validate();
-                    if (r) {
-                        return r;
+                    var i;
+                    var validators = [];
+                    for (i = 0; i < items.length; i++) {
+                        var item = items[i];
+                        if (!(item instanceof CartItem)) {
+                            return reject(util
+                                .format("%s[%d] is invalid.", key, i));
+                        }
+                        validators.push(item.validate());
                     }
-                }
+                    Promise.all(validators)
+                    .then(resolve)
+                    .catch(function(error) {
+                        reject(error.error);
+                    })
+                    ;
+                });
             },
         }],
     },
