2e4982b407564ad00db3771cd125004945f6edaf
Royal Pinto <royalpinto@gmail.com>
2016-09-05 15:21:00 +0530

    Added /users/id/ get service route function.

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,20 @@
 var urls = require('./../app/router');
 var router = new urls.Router();
+var controller = require('../controllers/user');
 
 
 
 
+router.get(/^\/users\/(\w+)\/$/, function(req, res) {
+    var id = req.params[0];
+    controller.getById(id)
+    .then(function(user) {
+        res.json(user);
+    })
+    .catch(function(error) {
+        errors.handle(req, res, error);
+    })
+    ;
 });
 
 
