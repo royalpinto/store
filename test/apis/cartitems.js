817edbb2ffe8788be46ae134989c06f32f51fe50
Royal Pinto <royalpinto@gmail.com>
2016-09-06 22:09:05 +0530

    Added cart/items/ service test case script.

---
+++ test/apis/cartitems.js
@@ -0,0 +1,54 @@
+/* global describe, it, before, beforeEach, afterEach */
+
+var chai = require('chai');
+var mongodb = require('mongodb');
+var config = require('./../../config');
+var models = require('./../../models');
+var server;
+
+
+chai.should();
+chai.use(require('chai-http'));
+
+
+describe('/cart/items/', function() {
+    before(function(done) {
+        mongodb.MongoClient.connect(config.db.uri)
+        .then(function(db) {
+            return models.init(db);
+        })
+        .then(function() {
+            return new Promise(function(resolve, reject) {
+                var port = config.server.port;
+                var listen = config.server.listen;
+                server = require('../../app');
+                server.listen(port, listen, function(err) {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve();
+                    }
+                });
+            });
+        })
+        .then(done)
+        .catch(done)
+        ;
+    });
+
+    var cleanCollection = function(done) {
+        Promise.all([
+            models.db.collection(models.User.collectionName).removeMany(),
+            models.db.collection(models.Permission.collectionName).removeMany(),
+        ])
+        .then(function() {
+            return models.Permission.add('admin', 'users', 'readwrite');
+        })
+        .then(done)
+        .catch(done)
+        ;
+    };
+
+    beforeEach(cleanCollection);
+    afterEach(cleanCollection);
+});
