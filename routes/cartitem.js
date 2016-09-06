41ca1a8d9112815571553384e94ebaf4a8fc2f79
Royal Pinto <royalpinto@gmail.com>
2016-09-06 19:44:43 +0530

    Added cartitem route with an initial service method to get the cart items.

---
+++ routes/cartitem.js
@@ -0,0 +1,23 @@
+var urls = require('./../app/router');
+var router = new urls.Router();
+var controller = require('./../controllers/cartitem');
+var errors = require('./../errors');
+var routes = require('./index');
+
+
+router.use(/^\/cart\/items\/$/, routes.handlePermission());
+
+router.get(/^\/cart\/items\/$/, function(req, res) {
+    controller
+    .get(req.session._id)
+    .then(function(cart) {
+        res.json(cart);
+    })
+    .catch(function(error) {
+        errors.handle(req, res, error);
+    })
+    ;
+});
+
+
+module.exports = router;
