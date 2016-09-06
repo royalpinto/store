11804f92156ea5df48a476abb8a6a653bf1649c1
Royal Pinto <royalpinto@gmail.com>
2016-09-06 19:42:19 +0530

    Implemented cartitem controller create method to add item to the cart.

--- controllers/cartitem.js
+++ controllers/cartitem.js
@@ -1,5 +1,6 @@
 var util = require('util');
 var models = require('./../models');
+var errors = require('./../errors');
 var Controller = require('./controller');
 
 
@@ -17,6 +18,42 @@ CartController.prototype.get = function(userId) {
             return cart.items;
         }
         return [];
+    })
+    ;
+};
+
+CartController.prototype.create = function(userId, projectId, quantity) {
+    console.log(userId, projectId, quantity);
+    var cart;
+    return models.Cart
+    .findByKey('userId', userId)
+    .then(function(_cart) {
+        if (_cart) {
+            cart = _cart;
+            return;
+        }
+        cart = new models.Cart({
+            userId: userId,
+            items: [],
+        });
+        return cart.save();
+    })
+    .then(function() {
+        if (cart.items.filter(function(item) {
+            return item.projectId.toString() === projectId;
+        }).length > 0) {
+            throw new errors.ValidationError("projectId already added.");
+        }
+        return models.Product.findById(projectId);
+    })
+    .then(function(product) {
+        if (!product) {
+            throw new errors.ValidationError("projectId invalid.");
+        }
+        if (product.quantity < quantity) {
+            throw new errors
+                .ValidationError("quantity is greater than available.");
+        }
     })
     ;
 };
