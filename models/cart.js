f0d7e8d0b7701d1d71d78dd0894f56dd894eec45
Royal Pinto <royalpinto@gmail.com>
2016-09-07 20:45:45 +0530

    Added getProducts method to Cart model.

--- models/cart.js
+++ models/cart.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var mongodb = require('mongodb');
 var Model = require('./model');
+var Product = require('./product');
 var CartItem = require('./cartitem');
 
 
@@ -46,6 +47,25 @@ util.inherits(Cart, Model);
 Object.assign(Cart, Model);
 
 Cart.setSchema(schema);
+
+Cart.prototype.getProducts = function() {
+    var productIds = this.items.map(function(item) {
+        return item.productId;
+    })
+    ;
+    return Product.collection.find({
+        _id: {
+            $in: productIds,
+        },
+    })
+    .toArray()
+    .then(function(docs) {
+        return docs.map(function(doc) {
+            return new Product(doc);
+        });
+    })
+    ;
+};
 
 
 module.exports = Cart;
