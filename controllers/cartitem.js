d055c129eff52b9b9b320a79039fe391db2c9401
Royal Pinto <royalpinto@gmail.com>
2016-09-07 14:56:22 +0530

    Added cartitem controller update method.

--- controllers/cartitem.js
+++ controllers/cartitem.js
@@ -62,5 +62,40 @@ CartController.prototype.create = function(userId, productId, quantity) {
     ;
 };
 
+CartController.prototype.update = function(userId, productId, quantity) {
+    var cart;
+    var cartItem = null;
+    return models.Cart
+    .findByKey('userId', userId)
+    .then(function(_cart) {
+        if (!_cart) {
+            throw new errors.ValidationError(
+                "productId not added to the cart.");
+        }
+        cart = _cart;
+        var i;
+        for (i = 0; i < cart.items.length; i++) {
+            if (cart.items[i].productId.toString() === productId) {
+                cartItem = cart.items[i];
+                break;
+            }
+        }
+        if (!cartItem) {
+            throw new errors.ValidationError(
+                "productId not added to the cart.");
+        }
+        return models.Product.findById(productId);
+    })
+    .then(function(product) {
+        if (product.quantity < quantity) {
+            throw new errors
+                .ValidationError("quantity is greater than available.");
+        }
+        cartItem.quantity = quantity;
+        return cart.save();
+    })
+    ;
+};
+
 
 module.exports = new CartController();
