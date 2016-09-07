152cf96762a235af5c4de6f3d6a7d6eb195f220d
Royal Pinto <royalpinto@gmail.com>
2016-09-07 22:04:49 +0530

    Updated models/index.js to include all models while importing (this way models can be referred before even calling init).

--- models/index.js
+++ models/index.js
@@ -1,33 +1,34 @@
 var fs = require('fs');
 var path = require('path');
 var models = {};
+var modelList = [];
+
+
+var files = fs.readdirSync(__dirname);
+for (var i = 0; i < files.length; i++) {
+    var file = files[i];
+    if (file.indexOf('.') !== 0 && file !== 'index.js' &&
+            file !== 'validators.js' && file.slice(-3) === '.js') {
+        var model = require(path.join(__dirname, file));
+        models[model.name] = model;
+        modelList.push(model);
+    }
+}
 
 
 models.init = function(db) {
     return new Promise(function(resolve, reject) {
         models.db = db;
-        fs.readdir(__dirname, function(err, files) {
-            if (err) {
-                return reject(err);
-            }
-            var initPromises = [];
-            files
-            .filter(function(file) {
-                return ((file.indexOf('.') !== 0) &&
-                        (file !== 'index.js') &&
-                        (file !== 'validators.js') &&
-                        (file.slice(-3) === '.js'));
-            })
-            .forEach(function(file) {
-                var model = require(path.join(__dirname, file));
-                models[model.name] = model;
-                initPromises.push(model.init(db));
-            });
+        var initPromises = [];
+        for (var i = 0; i < modelList.length; i++) {
+            var model = modelList[i];
+            initPromises.push(model.init(db));
+        }
 
-            return Promise.all(initPromises)
-            .then(resolve)
-            .catch(reject);
-        });
+        return Promise.all(initPromises)
+        .then(resolve)
+        .catch(reject)
+        ;
     });
 };
 
