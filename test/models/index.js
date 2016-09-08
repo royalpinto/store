1109661ae2433ef6e1d38135877d7e85f884da34
Royal Pinto <royalpinto@gmail.com>
2016-09-08 22:54:45 +0530

    Added test case to verify model.getDB.

--- test/models/index.js
+++ test/models/index.js
@@ -21,4 +21,24 @@ describe('models(Model):', function() {
         .catch(done)
         ;
     });
+
+    it("It should get the DB promise and resolve later.", function(done) {
+        models.db = null;
+
+        // Get the db first.
+        models.getDB()
+        .then(function() {
+            done();
+        })
+        .catch(done)
+        ;
+
+        mongodb.MongoClient.connect(config.db.uri)
+        .then(function(db) {
+            // This resolves db promises.
+            return models.init(db);
+        })
+        .catch(done)
+        ;
+    });
 });
