3357c78d5d2b49f8aa37fe00d5eeb85a7f2cca8c
Royal Pinto <royalpinto@gmail.com>
2016-09-09 20:26:10 +0530

    Added auth controller test script with initial register test method.

---
+++ test/controllers/auth.js
@@ -0,0 +1,57 @@
+/* global describe, it, before, beforeEach, afterEach */
+
+var chai = require('chai');
+var mongodb = require('mongodb');
+var config = require('./../../config');
+var models = require('./../../models');
+var controller = require('./../../controllers/auth');
+var errors = require('./../../errors');
+
+
+describe('CartItem(Controller):', function() {
+    before(function(done) {
+        mongodb.MongoClient.connect(config.db.uri)
+        .then(function(db) {
+            return models.init(db);
+        })
+        .then(function() {
+            done();
+        })
+        .catch(done)
+        ;
+    });
+
+    var cleanCollection = function(done) {
+        Promise.all([
+            models.User.collection.removeMany(),
+        ])
+        .then(function() {
+            done();
+        })
+        ;
+    };
+
+    beforeEach(cleanCollection);
+    afterEach(cleanCollection);
+
+    var payload = {
+        name: "Lohith Royal Pinto",
+        email: "royalpinto@gmail.com",
+        username: "royalpinto",
+        group: "member",
+        password: "password",
+    };
+
+    it("It should register user", function(done) {
+        controller
+        .registerUser(payload)
+        .then(function(user) {
+            chai.assert.isOk(user);
+            chai.assert.instanceOf(user, models.User);
+            chai.assert.equal(user.username, payload.username);
+            done();
+        })
+        .catch(done)
+        ;
+    });
+});
