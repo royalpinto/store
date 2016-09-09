af135e11726929cf54a19b618f349dbd3f2e88de
Royal Pinto <royalpinto@gmail.com>
2016-09-09 18:38:07 +0530

    Added cartitem controller test method to verify update item in the cart.

--- test/controllers/cartitem.js
+++ test/controllers/cartitem.js
@@ -134,4 +134,41 @@ describe('CartItem(Controller):', function() {
         .catch(done)
         ;
     });
+
+    it("It should update item quantity in the cart.", function(done) {
+        var user = new models.User(userpayload);
+        var product;
+        user.save()
+        .then(function() {
+            product = new models.Product(productpayload);
+            return product.save();
+        })
+        .then(function() {
+            var cart = new models.Cart({
+                userId: user._id,
+                items: [],
+            });
+            var cartitem = new models.CartItem({
+                productId: product._id,
+                quantity: 11,
+            });
+            cart.items.push(cartitem);
+            return cart.save();
+        })
+        .then(function() {
+            return controller.update(user._id, product._id, 4);
+        })
+        .then(function() {
+            return models.Cart.findByKey('userId', user._id);
+        })
+        .then(function(cart) {
+            chai.assert.isOk(cart);
+            chai.assert.isArray(cart.items);
+            chai.assert.lengthOf(cart.items, 1);
+            chai.assert.equal(cart.items[0].quantity, 4);
+            done();
+        })
+        .catch(done)
+        ;
+    });
 });
