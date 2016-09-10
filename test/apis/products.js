639c5f2470c70de64b94ec07cb9db6827fef2a34
Royal Pinto <royalpinto@gmail.com>
2016-09-10 18:17:31 +0530

    Added a test case to ensure internal error is handheld for GET /products/ service.

--- test/apis/products.js
+++ test/apis/products.js
@@ -86,6 +86,43 @@ describe('Products:', function() {
     });
 
     describe('/GET product', function() {
+        it('It should not get products for an internal error.', function(done) {
+            var backup;
+            var mock = function() {
+                backup = controller.get;
+                controller.get = function() {
+                    return Promise.reject(new Error("Mocked error :)"));
+                };
+            };
+            var demock = function() {
+                controller.get = backup;
+            };
+
+            controller.create(payload)
+            .then(function() {
+                mock();
+                return chai.request(server).get('/products/');
+            })
+            .then(function() {
+                demock();
+                done(true);
+            })
+            .catch(function(err) {
+                err.should.have.status(500);
+                chai.expect(err.response.text)
+                    .to.be.equal('Internal server error.');
+                demock();
+                done();
+            })
+            .catch(function() {
+                demock();
+                done(true);
+            })
+            ;
+        });
+    });
+
+    describe('/GET product', function() {
         it('It should not allow to limit > max allowed.', function(done) {
             var promises = [];
             for (var i = 0; i < 100; i++) {
