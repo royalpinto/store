c9d2df524532dc202f27a00c82f3962b0f205856
Royal Pinto <royalpinto@gmail.com>
2016-09-10 13:50:16 +0530

    Added test method to product apis to ensure error will be thrown for update with invalid id.

--- test/apis/products.js
+++ test/apis/products.js
@@ -60,6 +60,45 @@ describe('Products:', function() {
         });
     });
 
+    describe('/PUT product', function() {
+        it('It should not update a product for invalid id.', function(done) {
+            var agent = chai.request.agent(server);
+            agent.post('/register/')
+            .send({
+                name: "Lohith Royal Pinto",
+                email: "royalpinto@gmail.com",
+                username: "royalpinto",
+                password: "password",
+            })
+            .then(function(res) {
+                return models.User.findById(res.body._id);
+            })
+            .then(function(user) {
+                user.group = "admin";
+                return user.save();
+            })
+            .then(function() {
+                return agent
+                .put('/products/' + (new mongodb.ObjectID()).toString() + '/')
+                .send({
+                    name: "ABCDEDFGH",
+                })
+                ;
+            })
+            .then(function() {
+                done(true);
+            })
+            .catch(function(err) {
+                err.should.have.status(400);
+                chai.expect(err.response.body).to.have.property('error');
+                chai.expect(err.response.body.error)
+                    .to.be.equal("resource not found.");
+                done();
+            })
+            .catch(done);
+        });
+    });
+
     describe('/DELETE product', function() {
         it('It should not delete a product for invalid id.', function(done) {
             var agent = chai.request.agent(server);
