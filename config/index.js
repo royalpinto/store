3d7f2f546c915e6c3a3a1dbbe09123a6f008e5b6
Royal Pinto <royalpinto@gmail.com>
2016-09-10 20:30:46 +0530

    Updated config to consider development env if env is not specified.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,3 @@
-var env = process.env.NODE_ENV;
-if (!env || env === 'development') {
-    env = 'default';
-}
+var env = process.env.NODE_ENV || 'development';
 
 module.exports = require("./" + env);
