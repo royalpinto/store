a15b059471b9eef72e03a4239d6641e22e08a2dc
Royal Pinto <royalpinto@gmail.com>
2016-09-11 16:35:34 +0530

    Added logging module with winston logger and a http middleware.

---
+++ logging/index.js
@@ -0,0 +1,38 @@
+var winston = require('winston');
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.File)({
+            name: 'info',
+            filename: 'logs/info.log',
+            level: 'info',
+        }),
+        new (winston.transports.File)({
+            name: 'error',
+            filename: 'logs/error.log',
+            level: 'error',
+        }),
+        new (winston.transports.Console)({
+            colorize: true,
+        }),
+    ],
+});
+
+
+logger.middleware = function(req, res, next) {
+    res.on('finish', function() {
+        var username = req.session.user ? req.session.username : null;
+        if (res.statusCode >= 200 && res.statusCode < 300) {
+            return logger.info(req.method, req.url, res.statusCode, username);
+        }
+        if (res.statusCode >= 400 && res.statusCode < 500) {
+            return logger.warn(req.method, req.url, res.statusCode, username);
+        }
+        if (res.statusCode >= 500) {
+            return logger.error(req.method, req.url, res.statusCode, username);
+        }
+    });
+    next();
+};
+
+module.exports = logger;
