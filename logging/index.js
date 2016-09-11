ce92a76e23d5c22eed5f499084f6c257e8f13555
Royal Pinto <royalpinto@gmail.com>
2016-09-11 16:46:36 +0530

    Improved logic of parsing username from session for logging in logging module.

--- logging/index.js
+++ logging/index.js
@@ -21,7 +21,10 @@ var logger = new (winston.Logger)({
 
 logger.middleware = function(req, res, next) {
     res.on('finish', function() {
-        var username = req.session.user ? req.session.username : null;
+        var username = null;
+        if (req.session && req.session.user) {
+            username = req.session.user.username;
+        }
         if (res.statusCode >= 200 && res.statusCode < 300) {
             return logger.info(req.method, req.url, res.statusCode, username);
         }
