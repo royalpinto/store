d199545b99ed784f6c7001ddbd13d47b71f1c133
Royal Pinto <royalpinto@gmail.com>
2016-09-19 19:02:05 +0530

    Added products controller getCategories method.

--- controllers/product.js
+++ controllers/product.js
@@ -10,4 +10,39 @@ var ProductController = function() {
 util.inherits(ProductController, Controller);
 
 
+ProductController.prototype.getCategories =
+function(query, limit, skip, order) {
+    var pipeline = [{
+        $match: query,
+    }, {
+        $group: {
+            _id: '$category',
+        },
+    }, {
+        $project: {
+            name: '$_id', _id: 0,
+        },
+    }];
+
+    if (Object.keys(order).length === 0) {
+        order.name = 1;
+    }
+
+    pipeline.push({$sort: order});
+
+    pipeline.push({
+        $skip: skip,
+    });
+
+    pipeline.push({
+        $limit: limit,
+    });
+
+    return models.Product.collection
+    .aggregate(pipeline)
+    .toArray()
+    ;
+};
+
+
 module.exports = new ProductController();
