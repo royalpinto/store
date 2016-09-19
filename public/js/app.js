95e48759dbdbf4d9eec650911cd4ce0dd0a44af2
Royal Pinto <royalpinto@gmail.com>
2016-09-19 11:07:59 +0530

    Added initial browse controller with dummy values(will be replaced with actual implementation).

--- public/js/app.js
+++ public/js/app.js
@@ -45,4 +45,19 @@ angular
     },
 ])
 
+.controller('browseController', [
+    '$scope',
+    function($scope) {
+        $scope.filters = [{
+            name: 'Allen Solly',
+        }, {
+            name: 'Allen Solly',
+        }, {
+            name: 'Allen Solly',
+        }];
+        console.log(123);
+        $scope.products = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    },
+])
+
 ;
