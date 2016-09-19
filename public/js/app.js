a4efe27aca8f9f21038d2279351aa485bf4c84a7
Royal Pinto <royalpinto@gmail.com>
2016-09-19 09:47:33 +0530

    Configured client app.js initial set of routes.

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,28 @@
 angular
 
 .module('app', [
+    'ngRoute',
     'ngMaterial',
+])
+
+.config([
+    '$routeProvider',
+    function($routeProvider) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'views/browse.html',
+            controller: 'browseController',
+        })
+        .when('/product', {
+            templateUrl: 'views/product.html',
+            controller: 'productController',
+        })
+        .when('/cart', {
+            templateUrl: 'views/cart.html',
+            controller: 'cartController',
+        })
+        ;
+    },
 ])
 
 ;
