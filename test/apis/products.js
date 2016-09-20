91bb14247f11e7b5010a68444815d8a23124d72a
Royal Pinto <royalpinto@gmail.com>
2016-09-20 15:17:58 +0530

    Added test method for products/brands/ GET service.

--- test/apis/products.js
+++ test/apis/products.js
@@ -496,4 +496,20 @@ describe('Products:', function() {
             .catch(done);
         });
     });
+
+    describe('/api/products/brands/', function() {
+        it('It should get brands.', function(done) {
+            controller.create(payload)
+            .then(function() {
+                return chai.request(server).get('/api/products/brands/');
+            })
+            .then(function(res) {
+                res.should.have.status(200);
+                chai.expect(res.body).to.have.property('count');
+                chai.expect(res.body.count).to.be.equal(1);
+                done();
+            })
+            .catch(done);
+        });
+    });
 });
