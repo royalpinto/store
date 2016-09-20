8cd5a056f108b38c25ca20a2954fe2297999c4e7
Royal Pinto <royalpinto@gmail.com>
2016-09-20 14:16:23 +0530

    Updated user service get route to consider filter parameter and join with the search parameter if available.

--- routes/user.js
+++ routes/user.js
@@ -25,12 +25,12 @@ router.get(/^\/api\/users\/([a-fA-F\d]{24})\/$/, function(req, res) {
 });
 
 router.get(/^\/api\/users\/$/, function(req, res) {
-    var query = {};
+    var filter = req.query.filter;
     if (req.query.search) {
-        query.name = req.query.search;
+        filter.name = req.query.search;
     }
     controller
-    .get(query, req.query.limit, req.query.skip, req.query.order)
+    .get(filter, req.query.limit, req.query.skip, req.query.order)
     .then(function(user) {
         res.json(user);
     })
