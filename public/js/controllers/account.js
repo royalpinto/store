2b1a2c2de1ad203d6eb005c8e66071e2314f653a
Royal Pinto <royalpinto@gmail.com>
2016-09-22 14:18:28 +0530

    Added initial account view controller with login/register/logout features.

---
+++ public/js/controllers/account.js
@@ -0,0 +1,62 @@
+/* global angular */
+
+angular
+
+.module('app')
+
+.controller('accountController', [
+    '$scope',
+    '$http',
+    'Session',
+    '$mdDialog',
+    function($scope, $http, Session, $mdDialog) {
+        $scope.close = function() {
+            $mdDialog.hide();
+        };
+
+        $scope.logout = function() {
+            Session.destroy();
+            $scope.close();
+        };
+
+        $scope.user = Session;
+
+        $scope.login = {
+            do: function(data) {
+                $http.post('/api/login/', {
+                    username: data.username,
+                    password: data.password,
+                })
+                .then(function(response) {
+                    Session.create(response.data);
+                    $scope.close();
+                })
+                .catch(function(error) {
+                    $scope.login.error = error.data.error;
+                })
+                ;
+            },
+        };
+
+        $scope.register = {
+            do: function(data) {
+                $http.post('/api/register/', {
+                    username: data.username,
+                    password: data.password,
+                    name: data.name,
+                    email: data.email,
+                })
+                .then(function(response) {
+                    Session.create(response.data);
+                    $scope.close();
+                })
+                .catch(function(error) {
+                    $scope.register.error = error.data.error;
+                })
+                ;
+            },
+        };
+    },
+])
+
+;
