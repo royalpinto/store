d7ae80fcde4487767f92efda02f63b3116fc975c
Royal Pinto <royalpinto@gmail.com>
2016-09-24 22:22:06 +0530

    Added compare to directive to validate and compare two input models.

--- public/js/app.js
+++ public/js/app.js
@@ -72,6 +72,24 @@ angular
 ])
 
 
+.directive("compareTo", function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo",
+        },
+        link: function(scope, element, attributes, ngModel) {
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue === scope.otherModelValue;
+            };
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        },
+    };
+})
+
+
 .config([
     '$mdThemingProvider',
     function($mdThemingProvider) {
