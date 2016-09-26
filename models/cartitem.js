ebf9c26387aa75a40bce0792ce6c901cd5287c55
Royal Pinto <royalpinto@gmail.com>
2016-09-26 13:47:01 +0530

    Added documentation for CartItem Model.

--- models/cartitem.js
+++ models/cartitem.js
@@ -19,6 +19,17 @@ var schema = {
     },
 };
 
+/**
+ * Initialize a single cart item.
+ * @param {Object} [properties={}] The properties to be set to the cartitem.
+ * @param {String/ObjectID} [properties.productId] The product id.
+ * @param {Number} [properties.quantity] The product quantity.
+ * @class
+ * @extends models.Model
+ * @memberof models
+ * @classdesc Instances of the CartItem class represent a single cart item.
+ * This should be used as a sub document of Cart class and not as a main document.
+ */
 var CartItem = function CartItem(properties) {
     Model.call(this, properties);
 };
