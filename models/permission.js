b4720249a85fb7689c0a26063bb040df2a967743
Royal Pinto <royalpinto@gmail.com>
2016-09-26 13:47:17 +0530

    Added documentation for Permission Model.

--- models/permission.js
+++ models/permission.js
@@ -24,6 +24,18 @@ var schema = {
     },
 };
 
+/**
+ * Initialize a permission model with given properties.
+ * @param {Object} [properties={}] The properties to be set.
+ * @param {String} [properties.group] The group name.
+ * @param {String} [properties.noun] A noun.
+ * @param {String} [properties.verb] A verb.
+ * @class
+ * @extends models.Model
+ * @memberof models
+ * @classdesc Instances of the Permission class represent a single permission
+ * document.
+ */
 var Permission = function Permission(properties) {
     Model.call(this, properties);
 };
@@ -32,6 +44,14 @@ var Permission = function Permission(properties) {
 util.inherits(Permission, Model);
 Object.assign(Permission, Model);
 
+/**
+ * Add/Set permission.
+ * @param {String} group A group.
+ * @param {String} noun A noun.
+ * @param {String} verb A verb.
+ * @return {Promise} A promise which resolves upon success and rejects with an
+ * error upon failure.
+ */
 Permission.add = function(group, noun, verb) {
     var payload = {group: group, noun: noun, verb: verb};
     return Permission
@@ -48,6 +68,14 @@ Permission.add = function(group, noun, verb) {
     ;
 };
 
+/**
+ * Remove/Unset permission.
+ * @param {String} group A group.
+ * @param {String} noun A noun.
+ * @param {String} verb A verb.
+ * @return {Promise} A promise which resolves upon success and rejects with an
+ * error upon failure.
+ */
 Permission.remove = function(group, noun, verb) {
     var payload = {group: group, noun: noun, verb: verb};
     return Permission
@@ -62,6 +90,14 @@ Permission.remove = function(group, noun, verb) {
     ;
 };
 
+/**
+ * Check permission.
+ * @param {String} group A group.
+ * @param {String} noun A noun.
+ * @param {String} verb A verb.
+ * @return {Promise} A promise which resolves upon success and rejects with an
+ * error upon failure.
+ */
 Permission.check = function(group, noun, verb) {
     var payload = {group: group, noun: noun, verb: verb};
     return Permission
