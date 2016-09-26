8e061e8f1746b2483d76a5118a641b220a2c173f
Royal Pinto <royalpinto@gmail.com>
2016-09-26 13:47:33 +0530

    Added documentation for Product Model.

--- models/product.js
+++ models/product.js
@@ -80,6 +80,23 @@ var schema = {
     },
 };
 
+/**
+ * Initialize a product with given properties.
+ * @param {Object} [properties={}] The properties to be set to the product.
+ * @param {String} [properties.name] Name of the product.
+ * @param {String} [properties.code] The unique product code.
+ * @param {Number} [properties.price] The product price.
+ * @param {Number} [properties.quantity] The product quantity.
+ * @param {String} [properties.category] The product category.
+ * @param {String} [properties.brand] The product brand.
+ * @param {String} [properties.description] The product description.
+ * @param {Array} [properties.features] The list of features.
+ * @param {String} [properties.imgsrc] The imgsrc of the product.
+ * @class
+ * @extends models.Model
+ * @memberof models
+ * @classdesc Instances of the Product class represent a single product db document.
+ */
 var Product = function Product(properties) {
     Model.call(this, properties);
 };
