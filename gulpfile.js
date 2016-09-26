df6446c065872719629e8efcf0a8be6104e48f39
Royal Pinto <royalpinto@gmail.com>
2016-09-26 22:39:57 +0530

    Updated gulpfile to read env from arguments.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,15 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
 var concat = require('gulp-concat');
-var config = require('./config');
 var less = require('gulp-less');
+
+var env = 'prod';
+var _env = process.argv[process.argv.length - 1];
+if (_env && _env.startsWith('--')) {
+    env = _env.substring(2);
+}
+
+console.log(env);
 
 
 gulp.task('minifijs', function(cb) {
@@ -15,8 +22,8 @@ gulp.task('minifijs', function(cb) {
         concat('all.js'),
     ];
 
-    // On development env compression is applied for easy debug.
-    if (config.env !== 'development') {
+    // On development env compression is not applied for easy debug.
+    if (env !== 'development') {
         queue.push(uglify());
     }
 
