d654581cc2cadd4801846b8b1553cefb2acf3cdd
Royal Pinto <royalpinto@gmail.com>
2016-09-30 19:28:45 +0530

    Migrated routes/cartitem module to ES6.

--- routes/cartitem.js
+++ routes/cartitem.js
@@ -1,70 +1,73 @@
-var urls = require('./../app/router');
-var router = new urls.Router();
-var Controller = require('./../controllers/cartitem');
-var controller = new Controller();
-var errors = require('./../errors');
-var middlewares = require('./middlewares');
+'use strict';
+
+
+const urls = require('./../app/router');
+const router = new urls.Router();
+const Controller = require('./../controllers/cartitem');
+const controller = new Controller();
+const errors = require('./../errors');
+const middlewares = require('./middlewares');
 
 
 router.use(/^\/api\/cart\/items\/$/, middlewares.auth());
 router.use(/^\/api\/cart\/checkout\/$/, middlewares.auth());
 
-router.get(/^\/api\/cart\/items\/$/, function(req, res) {
+router.get(/^\/api\/cart\/items\/$/, (req, res) => {
     controller
     .get(req.session.user._id)
-    .then(function(cart) {
+    .then(cart => {
         res.json(cart);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.post(/^\/api\/cart\/items\/$/, function(req, res) {
+router.post(/^\/api\/cart\/items\/$/, (req, res) => {
     controller
     .create(req.session.user._id, req.body.productId, req.body.quantity)
-    .then(function() {
+    .then(() => {
         res.setHeader('Location', "/api/cart/items/");
         res.status(201).json({});
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.put(/^\/api\/cart\/items\/$/, function(req, res) {
+router.put(/^\/api\/cart\/items\/$/, (req, res) => {
     controller
     .update(req.session.user._id, req.body.productId, req.body.quantity)
-    .then(function() {
+    .then(() => {
         res.status(204).end();
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.delete(/^\/api\/cart\/items\/$/, function(req, res) {
+router.delete(/^\/api\/cart\/items\/$/, (req, res) => {
     controller
     .remove(req.session.user._id, req.query.productId)
-    .then(function() {
+    .then(() => {
         res.status(204).end();
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.post(/^\/api\/cart\/checkout\/$/, function(req, res) {
+router.post(/^\/api\/cart\/checkout\/$/, (req, res) => {
     controller
     .checkout(req.session.user._id)
-    .then(function() {
+    .then(() => {
         res.status(204).end();
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
