e25c9018260d2d598887ad3ba5a6ccf025dde3eb
Royal Pinto <royalpinto@gmail.com>
2016-09-30 21:42:51 +0530

    Migrated Cart model to ES6.

--- models/cart.js
+++ models/cart.js
@@ -1,12 +1,15 @@
-var util = require('util');
-var mongodb = require('mongodb');
-var Model = require('./model');
-var Product = require('./product');
-var CartItem = require('./cartitem');
-var validators = require('./validators');
+'use strict';
 
 
-var schema = {
+const util = require('util');
+const mongodb = require('mongodb');
+const Model = require('./model');
+const Product = require('./product');
+const CartItem = require('./cartitem');
+const validators = require('./validators');
+
+
+const schema = {
     userId: {
         type: mongodb.ObjectID,
         unique: true,
@@ -17,26 +20,25 @@ var schema = {
     items: {
         type: Array,
         validations: [{
-            fn: function(items, key) {
-                return new Promise(function(resolve, reject) {
+            fn: (items, key) => {
+                return new Promise((resolve, reject) => {
                     if (!(items instanceof Array)) {
                         return reject(util.format("%s is invalid.", key));
                     }
-                    var i;
-                    var validators = [];
-                    for (i = 0; i < items.length; i++) {
-                        var item = items[i];
+                    let validators = [];
+                    for (let i = 0; i < items.length; i++) {
+                        let item = items[i];
                         if (!(item instanceof CartItem)) {
                             item = items[i] = new CartItem(item);
                         }
                         validators.push(item.validate());
                     }
                     Promise.all(validators)
-                    .then(function() {
+                    .then(() => {
                         return items;
                     })
                     .then(resolve)
-                    .catch(function(error) {
+                    .catch(error => {
                         reject(error.error);
                     })
                     ;
@@ -56,38 +58,36 @@ var schema = {
  * @memberof models
  * @classdesc Instances of the Cart class represent a single user cart document.
  */
-var Cart = function Cart(properties) {
-    Model.call(this, properties);
-};
+class Cart extends Model {
 
-util.inherits(Cart, Model);
-Object.assign(Cart, Model);
+    /**
+     * Fetch products from the cart items.
+     * @return {Promise} A promise which resolves with list of `Product`s upon success
+     * and rejects with an error upon failure.
+     */
+    getProducts() {
+        let productIds = this.items.map(item => {
+            return item.productId;
+        })
+        ;
+        return Product.collection.find({
+            _id: {
+                $in: productIds,
+            },
+        })
+        .toArray()
+        .then(docs => {
+            return docs.map(doc => {
+                return new Product(doc);
+            });
+        })
+        ;
+    }
+
+}
+
 
 Cart.setSchema(schema);
-
-/**
- * Fetch products from the cart items.
- * @return {Promise} A promise which resolves with list of `Product`s upon success
- * and rejects with an error upon failure.
- */
-Cart.prototype.getProducts = function() {
-    var productIds = this.items.map(function(item) {
-        return item.productId;
-    })
-    ;
-    return Product.collection.find({
-        _id: {
-            $in: productIds,
-        },
-    })
-    .toArray()
-    .then(function(docs) {
-        return docs.map(function(doc) {
-            return new Product(doc);
-        });
-    })
-    ;
-};
 
 
 module.exports = Cart;
