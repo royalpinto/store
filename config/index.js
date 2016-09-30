2905f57cd5d5ce2852cbadd8346ae169dd492b58
Royal Pinto <royalpinto@gmail.com>
2016-09-30 17:28:24 +0530

    Migrated config module to ES6.

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
-var env = process.env.NODE_ENV || 'development';
+'use strict';
 
-var config = require("./" + env);
+
+const env = process.env.NODE_ENV || 'development';
+
+const config = require("./" + env);
 config.env = env;
 
 module.exports = config;
