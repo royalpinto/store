d63eb96333aca65a789f0a35c7d44cd6e4034ff3
Royal Pinto <royalpinto@gmail.com>
2016-09-30 18:34:39 +0530

    Migrated base controller class to ES6.

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,101 +1,110 @@
+'use strict';
+
+
 /**
  * @module controllers
  * @namespace controllers
  */
 
 
-var errors = require('./../errors');
+const errors = require('./../errors');
 
 /**
- * Initialize a controller.
- * @param {models.Model} Model The model on which controller should act.
  * @class
  * @memberof controllers
  * @classdesc A base controller class with very high level common methods.
  */
-var Controller = function(Model) {
-    this.Model = Model;
-};
+class Controller {
 
-/**
- * Create a model from the given data.
- * @param {Object} data The data to be set to the model.
- * @return {Promise} A promise which resolves upon model creation and rejects upon failure.
- */
-Controller.prototype.create = function(data) {
-    var model = new this.Model(data);
-    return model.save()
-    .then(function() {
-        return model;
-    });
-};
+    /**
+     * Initialize a controller.
+     * @param {models.Model} Model The model on which controller should act.
+     */
+    constructor(Model) {
+        this.Model = Model;
+    }
 
-/**
- * Fetch a model for the given id.
- * @param {String/ObjectID} id The model id.
- * @return {Promise} A promise which resolves with the retrived model.
- */
-Controller.prototype.getById = function(id) {
-    return this.Model
-    .findById(id)
-    .then(function(model) {
-        if (!model) {
-            throw new errors.ValidationError(
-                "resource not found.");
-        }
-        return model;
-    })
-    ;
-};
+    /**
+     * Create a model from the given data.
+     * @param {Object} data The data to be set to the model.
+     * @return {Promise} A promise which resolves upon model creation and rejects upon failure.
+     */
+    create(data) {
+        let model = new this.Model(data);
+        return model.save()
+        .then(() => {
+            return model;
+        });
+    }
 
-/**
- * Fetch models for the given input.
- * @param {Query} query Query according to the mongodb native nodejs driver spec.
- * @param {Number} limit Limit by number of  models to be fetched.
- * @param {Number} skip Skip number of models to be fetched.
- * @param {Order} order Order according to the mongodb native nodejs driver spec.
- * @return {Promise} A promise which resolves with the retrived models and count.
- */
-Controller.prototype.get = function(query, limit, skip, order) {
-    return this.Model.findAndCount(query, limit, skip, order);
-};
+    /**
+     * Fetch a model for the given id.
+     * @param {String/ObjectID} id The model id.
+     * @return {Promise} A promise which resolves with the retrived model.
+     */
+    getById(id) {
+        return this.Model
+        .findById(id)
+        .then(model => {
+            if (!model) {
+                throw new errors.ValidationError(
+                    "resource not found.");
+            }
+            return model;
+        })
+        ;
+    }
 
-/**
- * Update a model.
- * @param {String/ObjectID} id The model id.
- * @param {Object} data The data to be updated.
- * @return {Promise} A promise which resolves upon completion.
- */
-Controller.prototype.update = function(id, data) {
-    return this.Model
-    .findById(id)
-    .then(function(model) {
-        if (!model) {
-            throw new errors.ValidationError(
-                "resource not found.");
-        }
-        return model.update(data);
-    })
-    ;
-};
+    /**
+     * Fetch models for the given input.
+     * @param {Query} query Query according to the mongodb native nodejs driver spec.
+     * @param {Number} limit Limit by number of  models to be fetched.
+     * @param {Number} skip Skip number of models to be fetched.
+     * @param {Order} order Order according to the mongodb native nodejs driver spec.
+     * @return {Promise} A promise which resolves with the retrived models and count.
+     */
+    get(query, limit, skip, order) {
+        return this.Model.findAndCount(query, limit, skip, order);
+    }
 
-/**
- * Remove a model.
- * @param {String/ObjectID} id The model id.
- * @return {Promise} A promise which resolves upon completion.
- */
-Controller.prototype.remove = function(id) {
-    return this.Model
-    .findById(id)
-    .then(function(model) {
-        if (!model) {
-            throw new errors.ValidationError(
-                "resource not found.");
-        }
-        return model.remove();
-    })
-    ;
-};
+    /**
+     * Update a model.
+     * @param {String/ObjectID} id The model id.
+     * @param {Object} data The data to be updated.
+     * @return {Promise} A promise which resolves upon completion.
+     */
+    update(id, data) {
+        return this.Model
+        .findById(id)
+        .then(model => {
+            if (!model) {
+                throw new errors.ValidationError(
+                    "resource not found.");
+            }
+            return model.update(data);
+        })
+        ;
+    }
+
+    /**
+     * Remove a model.
+     * @param {String/ObjectID} id The model id.
+     * @return {Promise} A promise which resolves upon completion.
+     */
+    remove(id) {
+        return this.Model
+        .findById(id)
+        .then(model => {
+            if (!model) {
+                throw new errors.ValidationError(
+                    "resource not found.");
+            }
+            return model.remove();
+        })
+        ;
+    }
+
+}
 
 
 module.exports = Controller;
