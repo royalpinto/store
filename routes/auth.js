c27d2639d2ab39e6ea1b954be5cc4cbe8f115eff
Royal Pinto <royalpinto@gmail.com>
2016-09-30 19:28:10 +0530

    Migrated routes/auth module to ES6.

--- routes/auth.js
+++ routes/auth.js
@@ -1,51 +1,54 @@
-var urls = require('./../app/router');
-var router = new urls.Router();
-var middlewares = require('./middlewares');
-var Controller = require('./../controllers/user');
-var controller = new Controller();
-var errors = require('./../errors');
+'use strict';
+
+
+const urls = require('./../app/router');
+const router = new urls.Router();
+const middlewares = require('./middlewares');
+const Controller = require('./../controllers/user');
+const controller = new Controller();
+const errors = require('./../errors');
 
 
 router.get(/^\/api\/login\/$/, middlewares.auth());
 router.get(/^\/api\/logout\/$/, middlewares.auth());
 
 
-router.post(/^\/api\/login\/$/, function(req, res) {
+router.post(/^\/api\/login\/$/, (req, res) => {
     if (req.session.user) {
         return res.json(req.session.user);
     }
 
     controller
     .login(req.body.username, req.body.password)
-    .then(function(user) {
+    .then(user => {
         req.session.user = user.toJSON();
         res.json(user);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.get(/^\/api\/login\/$/, function(req, res) {
+router.get(/^\/api\/login\/$/, (req, res) => {
     res.json(req.session.user);
 });
 
-router.post(/^\/api\/register\/$/, function(req, res) {
+router.post(/^\/api\/register\/$/, (req, res) => {
     controller.create(req.body)
-    .then(function(user) {
+    .then(user => {
         req.session.user = user.toJSON();
         res.setHeader('Location', '/api/login/');
         res.status(201).json(user);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.get(/^\/api\/logout\/$/, function(req, res) {
-    req.session.destroy(function(error) {
+router.get(/^\/api\/logout\/$/, (req, res) => {
+    req.session.destroy(error => {
         if (error) {
             errors.handle(req, res, error);
         } else {
