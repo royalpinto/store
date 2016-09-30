ddc9d1ef62bea4fa91441f631415bb1fceeb1d51
Royal Pinto <royalpinto@gmail.com>
2016-09-30 19:28:56 +0530

    Migrated routes/product module to ES6.

--- routes/product.js
+++ routes/product.js
@@ -1,111 +1,114 @@
-var util = require('util');
-var urls = require('./../app/router');
-var router = new urls.Router();
-var Controller = require('../controllers/product');
-var controller = new Controller();
-var errors = require('./../errors');
-var middlewares = require('./middlewares');
+'use strict';
+
+
+const util = require('util');
+const urls = require('./../app/router');
+const router = new urls.Router();
+const Controller = require('../controllers/product');
+const controller = new Controller();
+const errors = require('./../errors');
+const middlewares = require('./middlewares');
 
 
 router.post(/^\/api\/products\//, middlewares.auth('products', 'write'));
 router.put(/^\/api\/products\//, middlewares.auth('products', 'write'));
 
 
-router.get(/^\/api\/products\/([a-fA-F\d]{24})\/$/, function(req, res) {
-    var id = req.params[0];
+router.get(/^\/api\/products\/([a-fA-F\d]{24})\/$/, (req, res) => {
+    let id = req.params[0];
     controller.getById(id)
-    .then(function(product) {
+    .then(product => {
         res.json(product);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.get(/^\/api\/products\/$/, function(req, res) {
-    var filter = req.query.filter;
+router.get(/^\/api\/products\/$/, (req, res) => {
+    let filter = req.query.filter;
     if (req.query.search) {
         filter.name = req.query.search;
     }
     controller
     .get(filter, req.query.limit, req.query.skip, req.query.order)
-    .then(function(product) {
+    .then(product => {
         res.json(product);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.post(/^\/api\/products\/$/, function(req, res) {
+router.post(/^\/api\/products\/$/, (req, res) => {
     controller
     .create(req.body)
-    .then(function(product) {
+    .then(product => {
         res.setHeader('Location',
             util.format("/api/products/%s/", product._id));
         res.status(201).json(product);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.put(/^\/api\/products\/([a-fA-F\d]{24})\/$/, function(req, res) {
-    var id = req.params[0];
+router.put(/^\/api\/products\/([a-fA-F\d]{24})\/$/, (req, res) => {
+    let id = req.params[0];
     controller
     .update(id, req.body)
-    .then(function() {
+    .then(() => {
         res.status(204).end();
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.delete(/^\/api\/products\/([a-fA-F\d]{24})\/$/, function(req, res) {
-    var id = req.params[0];
+router.delete(/^\/api\/products\/([a-fA-F\d]{24})\/$/, (req, res) => {
+    let id = req.params[0];
     controller
     .remove(id, req.body)
-    .then(function() {
+    .then(() => {
         res.status(204).end();
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.get(/^\/api\/products\/categories\//, function(req, res) {
-    var filter = req.query.filter;
+router.get(/^\/api\/products\/categories\//, (req, res) => {
+    let filter = req.query.filter;
     if (req.query.search) {
         filter.category = req.query.search;
     }
     controller
     .getCategories(filter, req.query.limit, req.query.skip, req.query.order)
-    .then(function(data) {
+    .then(data => {
         res.json(data);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
 });
 
-router.get(/^\/api\/products\/brands\//, function(req, res) {
-    var filter = req.query.filter;
+router.get(/^\/api\/products\/brands\//, (req, res) => {
+    let filter = req.query.filter;
     if (req.query.search) {
         filter.brand = req.query.search;
     }
     controller
     .getBrands(filter, req.query.limit, req.query.skip, req.query.order)
-    .then(function(data) {
+    .then(data => {
         res.json(data);
     })
-    .catch(function(error) {
+    .catch(error => {
         errors.handle(req, res, error);
     })
     ;
